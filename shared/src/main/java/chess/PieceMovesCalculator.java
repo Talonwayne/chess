008b3ff5ee238e17cea42a79e3a6841a80fdec20
Talonwayne <talonwayneanderson@gmail.com>
2024-09-10 17:43:00 -0600

    Adding missed files, Passed Chess Position

---
+++ shared/src/main/java/chess/PieceMovesCalculator.java
@@ -0,0 +1,40 @@
+package chess;
+
+public class PieceMovesCalculator{
+
+    public class KingMoves extends PieceMovesCalculator {
+        public KingMoves(ChessBoard board, ChessPosition position) {
+
+        }
+    }
+
+    public class QueenMoves extends PieceMovesCalculator {
+        public QueenMoves(ChessBoard board, ChessPosition position) {
+
+        }
+    }
+
+    public class KnightMoves extends PieceMovesCalculator {
+        public KnightMoves(ChessBoard board, ChessPosition position) {
+
+        }
+    }
+
+    public class BishopMoves extends PieceMovesCalculator {
+        public BishopMoves(ChessBoard board, ChessPosition position) {
+
+        }
+    }
+
+    public class RookMoves extends PieceMovesCalculator {
+        public RookMoves(ChessBoard board, ChessPosition position) {
+
+        }
+    }
+
+    public class PawnMoves extends PieceMovesCalculator {
+        public PawnMoves(ChessBoard board, ChessPosition position) {
+
+        }
+    }
+}
