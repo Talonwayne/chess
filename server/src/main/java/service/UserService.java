517819463a8b169961663ad40c17131a97d96a74
Talonwayne <talonwayneanderson@gmail.com>
2024-10-09 10:19:17 -0600

    Added Service and Handler Classes

--- server/src/main/java/service/UserService.java
+++ server/src/main/java/service/UserService.java
@@ -4,9 +4,7 @@ import model.AuthData;
 import model.UserData;
 
 public class UserService {
-    public AuthData register(UserData user) {
-
-    }
+    public AuthData register(UserData user) {}
     public AuthData login(UserData user) {}
     public void logout(AuthData auth) {}
 }
