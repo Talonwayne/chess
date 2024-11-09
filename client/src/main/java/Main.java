8c2b89cd6ff8d499a62a8b4a123479e5300f70a6
Talonwayne <talonwayneanderson@gmail.com>
2024-11-09 13:24:58 -0700

    Draw board

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -1,8 +1,14 @@
 import chess.*;
+import ui.DrawBoard;
 
 public class Main {
     public static void main(String[] args) {
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
         System.out.println("♕ 240 Chess Client: " + piece);
+        ChessGame game = new ChessGame();
+        DrawBoard whiteDisplay = new DrawBoard(true);
+        DrawBoard blackDisplay = new DrawBoard(false);
+        whiteDisplay.drawBoard(game);
+        blackDisplay.drawBoard(game);
     }
 }
--- client/src/main/java/ui/DrawBoard.java
+++ client/src/main/java/ui/DrawBoard.java
@@ -12,52 +12,71 @@ public class DrawBoard {
 
     private boolean isWhite;
 
-    public DrawBoard(boolean isWhite){
+    public DrawBoard(boolean isWhite) {
         this.isWhite = isWhite;
     }
 
-    public void drawBoard(ChessGame game){
-        String[] blackSymbols = {"a","b","c","d","e","f","g","h"};
-        String[] whiteSymbols = {"h","g","f","e","d","c","b","a"};
-
-        if (isWhite){
-            String[] symbols = {"h","g","f","e","d","c","b","a"};
-            drawMargin(symbols);
-
-            drawMargin(symbols);
-
-        } else {
-            String[] symbols = {"a","b","c","d","e","f","g","h"};
-            drawMargin(symbols);
-
-            drawMargin(symbols);
-
-        }
-
+    public void setWhite(boolean isViewColorWhite) {
+        isWhite = isViewColorWhite;
     }
 
-    public void drawMargin(String[] symbols){
-        System.out.print(BG);
-        for (String symbol: symbols){
-            System.out.print(symbol);
+    public void drawBoard(ChessGame game) {
+        String[] symbols = isWhite ? new String[]{"a", "b", "c", "d", "e", "f", "g", "h"} : new String[]{"h", "g", "f", "e", "d", "c", "b", "a"};
+        drawMargin(symbols);
+        for (int row = 1; row <= NUM_SQUARES_XY; row++) {
+            drawRow(row,game);
+            System.out.println();
         }
+        drawMargin(symbols);
     }
 
-    public void drawRow(int row, ChessGame game){
+    public void drawMargin(String[] symbols) {
         System.out.print(BG);
-        System.out.print(row);
-        for (int i = 1; i < 9; i++){
-            ChessPosition square = new ChessPosition(row,i);
+        for (String symbol : symbols) {
+            System.out.print(" " + symbol + " ");
+        }
+        System.out.println();
+    }
+
+    public void drawRow(int row, ChessGame game) {
+        System.out.print(row + " ");
+        for (int col = 1; col <= NUM_SQUARES_XY; col++) {
+            ChessPosition square = new ChessPosition(row, col);
             ChessPiece piece = game.getBoard().getPiece(square);
-            drawSquare(square,piece);
+            drawSquare(square, piece);
         }
-        System.out.print(BG);
-        System.out.print(row);
+        System.out.print(" " + row);
     }
 
-    public void drawSquare(ChessPosition square, ChessPiece piece){
-        if (square.getColumn() % 2 == 1){
-            System.out.print();
+    public void drawSquare(ChessPosition square, ChessPiece piece) {
+        String squareColor = (square.getRow() + square.getColumn()) % 2 == 0 ? WHITE_SQUARE : BLACK_SQUARE;
+        System.out.print(squareColor);
+        ChessPiece.PieceType pieceType = piece.getPieceType();
+        if (piece.equals(null)) {
+            System.out.print(EscapeSequences.EMPTY);
+            return;
         }
+        if (piece.getTeamColor() == ChessGame.TeamColor.WHITE){
+            switch (pieceType) {
+                case PAWN ->   System.out.print(EscapeSequences.WHITE_PAWN);
+                case ROOK ->   System.out.print(EscapeSequences.WHITE_ROOK);
+                case BISHOP -> System.out.print(EscapeSequences.WHITE_BISHOP);
+                case QUEEN ->  System.out.print(EscapeSequences.WHITE_QUEEN);
+                case KNIGHT -> System.out.print(EscapeSequences.WHITE_KNIGHT);
+                case KING ->   System.out.print(EscapeSequences.WHITE_KING);
+                case null, default -> System.out.print(EscapeSequences.EMPTY);
+            }
+        } else {
+            switch (pieceType) {
+                case PAWN ->   System.out.print(EscapeSequences.BLACK_PAWN);
+                case ROOK ->   System.out.print(EscapeSequences.BLACK_ROOK);
+                case BISHOP -> System.out.print(EscapeSequences.BLACK_BISHOP);
+                case QUEEN ->  System.out.print(EscapeSequences.BLACK_QUEEN);
+                case KNIGHT -> System.out.print(EscapeSequences.BLACK_KNIGHT);
+                case KING ->   System.out.print(EscapeSequences.BLACK_KING);
+                case null, default -> System.out.print(EscapeSequences.EMPTY);
+            }
+        }
+        System.out.print(EscapeSequences.RESET_BG_COLOR);
     }
 }
